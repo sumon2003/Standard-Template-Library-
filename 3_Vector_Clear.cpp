7f814f096f214861fc0fa815f84d36757fe31dfa
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-23 19:15:40 +0600

    Use of clear()

---
+++ 3_Vector_Clear.cpp
@@ -0,0 +1,25 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan  //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> v;
+    
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(5);
+
+    for(auto u : v) cout << u << " ";
+    cout << endl;
+    
+    // Clear the vector using clear().
+    v.clear();
+
+    cout << v.size() << endl;
+
+
+
+}    

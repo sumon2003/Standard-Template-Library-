cf70c66b11206a513dd0cc801eb1c96c6ebe4842
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-23 19:14:37 +0600

    Use of size()

---
+++ 2_Vector_Size.cpp
@@ -0,0 +1,23 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan  //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> v;
+    
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(5);
+
+// size() = We can determine the vector size.
+    cout << v.size() << endl;
+
+    for(int i=0; i<v.size(); i++) cout << v[i] << " ";
+    cout << endl;
+        
+
+
+}

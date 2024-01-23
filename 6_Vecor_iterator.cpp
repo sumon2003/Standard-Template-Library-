67389d4be2fda792922cf9ff5edc816d9d467ef1
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-23 19:24:25 +0600

    Vector Iterator

---
+++ 6_Vecor_iterator.cpp
@@ -0,0 +1,17 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan  //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> v{1, 2, 3, 4, 5};
+    vector <int> :: iterator it; // Here 'it' is a variable name.
+    
+    for(it = v.begin(); it != v.end(); it++) cout << *it << " ";
+    cout << endl;
+
+
+
+}

20ce8750acb800f3a0ea76f42c22483c70bab9ec
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-23 19:17:07 +0600

    Use of empty()

---
+++ 4_Vector_Empty_Function.cpp
@@ -0,0 +1,19 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan  //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> v1, v2{1, 2, 3, 4, 5};
+
+    cout << v2.size() << endl;
+// empty() check, a vector is empty or not? If a vector is emppty it returns 1, else 0.
+    cout << v1.empty() << endl;
+    cout << v2.empty() << endl;
+
+
+
+
+}

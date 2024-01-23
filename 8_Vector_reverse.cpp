fc580a2c7862519e2f215e9d115aaf02efef9529
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-23 20:38:44 +0600

    Use of reverse()

---
+++ 8_Vector_reverse.cpp
@@ -0,0 +1,18 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan  //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <int> v{40, 10, 30, 20, 50};
+
+    reverse(v.begin(), v.end());
+
+    for(auto u : v) cout << u << " ";
+    cout << endl;
+
+
+
+}

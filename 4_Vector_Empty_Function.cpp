7f9f23741d497d87ee2908a2c1b281a3e863f2f3
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-24 21:02:39 +0600

    Vector_Empty_Function

--- 4_Vector_Empty_Function.cpp
+++ 4_Vector_Empty_Function.cpp
@@ -9,7 +9,7 @@ int main()
     vector <int> v1, v2{1, 2, 3, 4, 5};
 
     cout << v2.size() << endl;
-// empty() check, a vector is empty or not? If a vector is emppty it returns 1, else 0.
+    // empty() check, a vector is empty or not? If a vector is emppty it returns 1, else 0.
     cout << v1.empty() << endl;
     cout << v2.empty() << endl;
 
